94586738914078217cafe82e980600b5a56e2856
Shubham Garg <gargshubham21156@gmail.com>
2023-07-11 18:22:01 +0530

    Type Aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,40 +1,19 @@
-let greet = () => {
-    console.log('hello, world');
+const logDetails = (uid : string | number, item : string) => {
+    console.log(`${item} has uid of ${uid}`)
 }
 
-// greet = 'hello'; //cannot be changed to string
-
-let fun : Function; //explicitly defined 
-
-fun = () => {
-    console.log('hello');
+const greet = (user : {name : string, uid : string | number}) => {
+    console.log(`${user.name}`);
 }
 
-// -------------------- //
-const add = (a : number, b : number, c?: number | string) => {
-    console.log(a + b);
-    console.log(c); //if c is not passed, it is undefined as  c is optional parameter
+//Here we are using string | number type again and again, so we can use type aliases
+type StringOrNum = string | number;
+const log = (uid : StringOrNum, item : string) => {
+    console.log(`${item} has uid of ${uid}`)
 }
 
-add(1,2);
-
-// --------------- //
-const add2 = (a : number, b : number, c: number | string = 10) => {
-    console.log(a + b);
-    console.log(c); //if c is not passed, it will have value 10
-}
-
-// ----------------- //
-
-const minus = (a:number, b:number) => {
-    return a-b;
-}
-
-let result = minus(10,7); //type of the variable result will be decided by the value returned by the function.
-
-//As 'result' is a number (as number is returned by 'minus'), it cannot be changed to string or boolean. 
-
-// ------------------ //
-const minus2 = (a:number, b:number) : number => {  //we can explicitly define return type of the function. 
-    return a-b;
+//we can make type aliases of object too
+type objWithName = { name : string, uid : StringOrNum};
+const greetAgain = (user : objWithName) => {
+    console.log(`${user.name}`);
 }
