9f4dadbeb0691a07290262b18e7f2a82b4d6b54b
Shubham Garg <gargshubham21156@gmail.com>
2023-07-15 15:19:44 +0530

    Function signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,19 +1,38 @@
-const logDetails = (uid : string | number, item : string) => {
-    console.log(`${item} has uid of ${uid}`)
+let fun = Function;
+
+//example 1
+let greet : ( a : string, b : string) => void; //Function signature
+
+greet = (name:string, msg :string) => {
+    console.log(`${name} says ${msg}`);
 }
 
-const greet = (user : {name : string, uid : string | number}) => {
-    console.log(`${user.name}`);
+//example 2
+let calc : (a : number, b : number, c : string) => number;
+
+calc = (numOne : number, numTwo : number, action : string) => {
+    if(action === 'add'){
+        return numOne + numTwo;
+    }else{
+        return numOne - numTwo;
+    }
 }
 
-//Here we are using string | number type again and again, so we can use type aliases
-type StringOrNum = string | number;
-const log = (uid : StringOrNum, item : string) => {
-    console.log(`${item} has uid of ${uid}`)
+//example 3
+let log : (obj : {name : string, age : number}) => void;
+log = (obj : {name : string , age : number}) => {
+    console.log(`${obj.name} , ${obj.age}`);
 }
 
-//we can make type aliases of object too
-type objWithName = { name : string, uid : StringOrNum};
-const greetAgain = (user : objWithName) => {
-    console.log(`${user.name}`);
+//we can also use type aliases
+type person = {name : string , age : number};
+
+log = (obj : person) => {
+    console.log(`${obj.name} , ${obj.age}`);
 }
+
+
+
+
+
+
